500a334703df3c7950e6a24078970dcc26a5524b
Mohamed Mhamdi <mhamdi.mohamed944@gmail.com>
2020-12-14 14:41:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import myImage from './imageInSrc.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName : "Naruto Uzumaki",
+      bio : "I just love ramen",
+      imgSrc : myImage,
+      profession : "7th Hokage"
+    },
+    show : true,
+    counter : 0
+  };
+  handleShowPerson = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({counter: this.state.counter + 1})
+    }, 1000)
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.show && (
+          <>
+            <h1>Full Name : {this.state.Person.fullName}</h1>
+            <h1>My Bio : {this.state.Person.bio}</h1>
+            <h1>My Profession : {this.state.Person.profession}</h1>
+            <img src={this.state.Person.imgSrc} alt="Naruto" height="460"></img>
+            <br></br>
+          </>
+        )}
+        <button onClick={this.handleShowPerson}>CLICK ME</button>
+        <p>{this.state.counter}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
